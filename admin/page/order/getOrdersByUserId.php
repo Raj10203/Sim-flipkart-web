2d7aac7ff75dc1a8e9faf0467502984d5ea0dd66
Raj10203 <patilricky39@gmail.com>
2025-04-07 15:09:39 +0530

    removed error handlers

--- admin/page/order/getOrdersByUserId.php
+++ admin/page/order/getOrdersByUserId.php
@@ -1,7 +1,5 @@
 <?php
-ini_set('display_errors', '1');
-ini_set('display_startup_errors', '1');
-error_reporting(E_ALL);
+
 require_once('../../authentication/backend_authenticate.php');
 require_once('../../classes/traits/ItemOperations.php');
 require_once('../../classes/Database.php');
